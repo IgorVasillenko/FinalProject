795fdaffdec68362615c0d0a48a3a341342a331c
amitosm <amitmargalit1@gmail.com>
2021-11-28 18:05:33 +0200

    trying new route

--- routes.py
+++ routes.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/meow")
+def hello_world():
+    return "<p>Hello, Amit!</p>"
+
 if __name__ == '__main__':
     app.run()
