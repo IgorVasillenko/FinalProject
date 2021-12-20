aa0fb5fd8785db1c542d33508d51717acaab6593
amitosm <amitmargalit1@gmail.com>
2021-12-20 14:53:18 +0200

    Update db_functions.py

--- db_queries/db_functions.py
+++ db_queries/db_functions.py
@@ -4,10 +4,12 @@ In this file, we will have all our necessary  DB functions:
 inert, find , update and delete.
 our global variables are:
  1. client -> our connection to mongoDB
- 2. DB -> our connection to fave-recognition-db DB
+ 2. DB -> our connection to face-recognition-db DB
 '''
 # client is the connection
-client = pymongo.MongoClient("mongodb+srv://admin:igoramit0987@cluster0.c2drp.mongodb.net/face-recognition-db?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://admin:igoramit0987@cluster0.c2drp.mongodb.net/face-recognition-db?retryWrites=true&w=majority")
+
 DB = client['face-recognition-db']
 
 def insert_one(collection:str, values:dict):
@@ -37,18 +39,37 @@ def delete_many(collection:str, query:dict):
 
 
 def update_one(collection:str, query:dict, newValues:dict):
-    '''
-
+    """
     :param collection:
     :param query:
     :param newValues: new values should be in this format: {"$set": { "address": "Canyon 123" } }
     :return:
-    '''
+    """
     connection = DB[collection]
     return connection.update_one(query, newValues)
 
+def update_one_and_return(collection:str, query:dict, newValues:dict):
+    # this function is same as updade_one.
+    # the only differ is that the function returns the updated document.
+    connection = DB[collection]
+    return connection.find_one_and_update(query, newValues)
 
-insert_one('kids', {"name":"amit", "age":"20"})
+def update_many(collection:str, query:dict, newValues:dict):
+    connection = DB[collection]
+    connection.update_many(query, newValues)
+
+
+def find_one(collection:str, query:dict=None):
+    """
+    :param collection:  the collection we want to find form
+    :param query: if we don't want any query, the default value is none.
+    :return: one document that follows the given query if given.
+    """
+
+    connection = DB[collection]
+    return connection.find_one(query)
+
+
 # delete_one('kids', {"name":"amit"})
 # insert_many('kids',[{"name":"amit", "age":"20"},{"name":"amit", "age":"20"},{"name":"amit", "age":"20"}])
 # delete_many('kids',{"name":"amit", "age":"20"})
