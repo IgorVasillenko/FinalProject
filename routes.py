4ff1f42f61e3b8fe96415b41e4a8136a72357898
amitosm <amitmargalit1@gmail.com>
2021-12-22 18:14:55 +0200

    uncommenting sections

--- routes.py
+++ routes.py
@@ -8,17 +8,17 @@ def home():
     return render_template('subfolder/home.html')
 
 
-# @app.route("/login", methods=["POST"])
-# def post_login_info():
-#     if check_login(request.form.to_dict()):
-#         return render_template('subfolder/mainPage.html')
-#     else:
-#         message = "Wrong username or password"
-#         return render_template('subfolder/home.html', message=message)
+@app.route("/login", methods=["POST"])
+def post_login_info():
+    if check_login(request.form.to_dict()):
+        return render_template('subfolder/mainPage.html')
+    else:
+        message = "Wrong username or password"
+        return render_template('subfolder/home.html', message=message)
 
-# @app.route("/register", methods =["GET"])
-# def register():
-#     return "hello world"
+@app.route("/register", methods =["GET"])
+def register():
+    return "hello world"
 
 
 if __name__ == '__main__':
