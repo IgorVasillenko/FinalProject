b422206300b2c24b528eec12bc61f8581d148d5c
amitosm <amitmargalit1@gmail.com>
2022-05-15 17:05:29 +0300

    Update clock.py

--- clock.py
+++ clock.py
@@ -10,9 +10,9 @@ def timed_job():
     print('This job will run every three minutes.')
 
 
-@sched.scheduled_job('cron', hour='16', minute='52')
+@sched.scheduled_job('cron', hour='17', minute='09')
 def schedule_for_today():
-    print("schedule works 17:07")
+    print("schedule works 17:09")
     teachers = find_all('managers',{})
     clean_teachers =handle_cursor_obj(teachers)
     for teacher in clean_teachers:
@@ -20,9 +20,9 @@ def schedule_for_today():
         # sched.add_job(my_job, trigger='date', hour='01', minute='05')
 
 
-@sched.scheduled_job('cron', hour='13', minute='52')
+@sched.scheduled_job('cron', hour='14', minute='09')
 def schedule_for_today2():
-    print("schedule works 14:07")
+    print("schedule works 14:09")
     teachers = find_all('managers',{})
     clean_teachers =handle_cursor_obj(teachers)
     for teacher in clean_teachers:
