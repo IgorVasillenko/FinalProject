11cb4c5c8ced86731c6bb0d2bf02f03b7e6adf90
amitosm <amitmargalit1@gmail.com>
2022-05-15 16:30:06 +0300

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,10 +1,28 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from db_queries.db_functions import *
+from main import *
 
-sched = BlockingScheduler()
+
+sched = BlockingScheduler({'apscheduler.timezone': 'GMT'})
 
 @sched.scheduled_job('interval', minutes=2)
 def timed_job():
     print('This job will run every three minutes.')
+
+
+@sched.scheduled_job('cron', hour='01', minute='05')
+def schedule_for_today():
+    teachers = find_all('managers',{})
+    clean_teachers =  handle_cursor_obj(teachers)
+    for teacher in clean_teachers:
+        print(teacher)
+        # sched.add_job(my_job, trigger='date', hour='01', minute='05')
+
+
+# will run everyday at 01:05 AM.
+# sched.add_job(my_job, trigger='cron', hour='01', minute='05')
+
+@sched.scheduled_job('interval', minutes=2)
 
 
 sched.start()
