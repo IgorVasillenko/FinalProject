39507c210095334726bab8da8ca386509babf3e1
amitosm <amitmargalit1@gmail.com>
2022-05-19 16:14:14 +0300

    produce again option

--- clock.py
+++ clock.py
@@ -21,7 +21,7 @@ def schedule_for_today():
         execute_date, db_date_format = handle_schedule_dates(teacher["schedule"])
         print("TEACHER SCHEDULE IS AT: ", teacher["schedule"])
         sched.add_job(create_attendance_report, trigger='date', run_date=execute_date,
-                      args=[class_name, db_date_format])
+                      args=[class_name, db_date_format], id=class_name)
     print("ADDED TASKS:")
     print(sched.get_jobs())
 
